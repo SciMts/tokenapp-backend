f9a193c5f439fc3ef73649829b5f7a6e5c5364ec
Jonny Burger <jonathanburger11@gmail.com>
2018-03-01 17:05:17 +0100

    cross origin

--- services/backend/src/main/java/io/modum/tokenapp/backend/integration/CorsConfiguration.java
+++ services/backend/src/main/java/io/modum/tokenapp/backend/integration/CorsConfiguration.java
@@ -9,6 +9,6 @@ public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET").allowedOrigins("https://buy.eurekatoken.io");
+        registry.addMapping("/**").allowedMethods("GET").allowedOrigins("https://buy.eurekatoken.io", "http://localhost:8080");
     }
 }
