2867f8f13811fd341e7a1179aeff378899977075
Jonny Burger <jonathanburger11@gmail.com>
2018-03-01 17:06:11 +0100

    options

--- services/backend/src/main/java/io/modum/tokenapp/backend/integration/CorsConfiguration.java
+++ services/backend/src/main/java/io/modum/tokenapp/backend/integration/CorsConfiguration.java
@@ -9,6 +9,6 @@ public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET").allowedOrigins("https://buy.eurekatoken.io", "http://localhost:8080");
+        registry.addMapping("/**").allowedMethods("GET", "OPTIONS").allowedOrigins("https://buy.eurekatoken.io", "http://localhost:8080");
     }
 }
